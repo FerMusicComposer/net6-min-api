f0bf610170cf3892970a1425a1396388accf8dac
Fernando Salamanca <67641798+FerMusicComposer@users.noreply.github.com>
2024-03-09 17:19:40 +0100

    created all app endpoints added SqlClient package

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using AutoMapper;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using SixMinAPI.Data;
 using SixMinAPI.Repositories;
+using SixMinAPI.Dtos;
+using SixMinAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -33,6 +36,56 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Application Endpoints
+app.MapGet("api/v1/commands", async (ICommandRepo repo, IMapper mapper) =>
+{
+    var commands = await repo.GetAllCommands();
+    return Results.Ok(mapper.Map<IEnumerable<CommandReadDto>>(commands));
+});
+
+app.MapGet("api/v1/commands/{id}", async (ICommandRepo repo, IMapper mapper, int id) =>
+{
+    var command = await repo.GetCommandById(id);
+    if (command != null)
+    {
+        return Results.Ok(mapper.Map<CommandReadDto>(command));
+    }
+
+    return Results.NotFound();
+});
+
+app.MapPost("api/v1/commands", async (ICommandRepo repo, IMapper mapper, CommandCreateDto commandCreateDto) =>
+{
+    var command = mapper.Map<Command>(commandCreateDto);
+    await repo.CreateCommand(command);
+
+    return Results.Created($"api/v1/commands/{command.Id}", mapper.Map<CommandReadDto>(command));
+});
+
+app.MapPut("api/v1/commands/{id}", async (ICommandRepo repo, IMapper mapper, int id, CommandUpdateDto commandUpdateDto) =>
+{
+    var command = await repo.GetCommandById(id);
+    if (command == null)
+    {
+        return Results.NotFound();
+    }
+    mapper.Map(commandUpdateDto, command);
+    await repo.SaveChanges();
+
+    return Results.Created($"api/v1/commands/{command.Id}", mapper.Map<CommandReadDto>(command));
+});
+
+app.MapDelete("api/v1/commands/{id}", async (ICommandRepo repo, int id) =>
+{
+    var command = await repo.GetCommandById(id);
+    if (command == null)
+    {
+        return Results.NotFound();
+    }
+    repo.DeleteCommand(command);
+    await repo.SaveChanges();
+    return Results.Ok($"Command with id {id} deleted");
+});
 
 app.Run();
 
